689151175ac3ef93ef0034eff7686b6ced45092a
Maryam Taheri <31124414+MaryamTaheri@users.noreply.github.com>
2022-07-03 01:44:08 -0700

    Update OrderApiControllerTest.cs

--- Online_Shop.Tests/OrderApiControllerTest.cs
+++ Online_Shop.Tests/OrderApiControllerTest.cs
@@ -21,18 +21,18 @@ namespace Online_Shop.Tests
         public void Success_Create_Order_Test()
         {
             //Arrange
-            var mediator = new Mock<IOrderCommandModel>();
+            //var mediator = new Mock<IOrderCommandModel>();
 
-            CreateOrderCommand createOrderCommand = new CreateOrderCommand { };  //with fake data
+            //CreateOrderCommand createOrderCommand = new CreateOrderCommand { };  //with fake data
 
-            OrderCommands commands = new OrderCommands(mediator.Object);
+            //OrderCommands commands = new OrderCommands(mediator.Object);
 
             //Act
-            var result = await commands.Handle(createOrderCommand, new System.Threading.CancellationToken());
+            //var result = await commands.Handle(createOrderCommand, new System.Threading.CancellationToken());
 
             //Asert
             //Do the assertion
-            var expectedResult = mockDataBase.Value.OutputData;
+            //var expectedResult = mockDataBase.Value.OutputData;
             // result.StatusCode.Should().Be(expectedResult.StatusCode);
             // exceptionResult.Result.Status.Should().Be(expectedResult.Result.Status);
             // exceptionResult.Result.Message.Should().Contain(expectedResult.Result.Message);
